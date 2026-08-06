aa00fc05db6ab59beb4e612191c3f384b39bb965
bhaveshwalode-collab <bhaveshwalode@gmail.com>
2026-08-07 01:22:35 +0530

    Connect frontend to Render backend

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://voting-app-backend-r5nr.onrender.com',
         changeOrigin: true,
         secure: false,
       }
